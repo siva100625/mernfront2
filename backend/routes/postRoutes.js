0083307e667f84dcaaf1dd2157529da53b3597cf
siva100625 <153293464+siva100625@users.noreply.github.com>
2025-01-30 09:57:44 +0530

    Delete backend directory

--- backend/routes/postRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createPost, getAllPosts } = require("../controllers/postController.js");
-const protect = require("../middleware/authMiddleware.js");
-
-const router = express.Router();
-router.post("/create", protect, createPost);
-router.get("/", getAllPosts);
-
-module.exports = router;
--- backend/routes/replyRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createReply, getRepliesByPost } = require("../controllers/replyController.js");
-const protect = require("../middleware/authMiddleware.js");
-
-const router = express.Router();
-router.post("/create", protect, createReply);
-router.get("/:postId", getRepliesByPost);
-
-module.exports = router;
